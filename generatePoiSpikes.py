53edffaf57f85b7bad106207390f3966750e78ec
hilaman1 <hilaman1@mail.tau.ac.il>
2022-01-05 18:14:38 +0200

    update 18:14

--- generatePoiSpikes.py
+++ generatePoiSpikes.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def generatePoiSpikes(r, dt, totalSize):
     if isinstance(r, np.ndarray):
-        spike_train = np.Generator.poisson(r, totalSize / dt)
+        spike_train = np.Generator.poisson(r, int(totalSize / dt))
     else:
-        spike_train = np.random.poisson(r, totalSize / dt)
+        spike_train = np.random.poisson(r, int(totalSize / dt))
     return spike_train
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import numpy as np
+from generatePoiSpikes import generatePoiSpikes
+import matplotlib.pyplot as plt
+
+# Question 2.1
+spike_train = generatePoiSpikes(30, 0.001, 60*1000)
+count, bins, ignored = plt.hist(spike_train, 20, density=True)
+plt.xlabel('Time (ms)', fontsize=16)
+plt.ylabel('Spike', fontsize=16)
+plt.show()
+
+# Question 2.2
+firing_rate = [30] + [0 for i in range(1, 60000)]
+spike_train = [np.random.poisson(lam=firing_rate[0], size=None)]+[0 for i in range(1, 60000)]
+t_spike = 0
+for t in range(1, 60*1000):
+    firing_rate[t] = firing_rate[0]*(t - t_spike)/5
+    spike_train = np.random.poisson(lam=t-1, size=None)
+
+
+
+
+
+
+
+
 
 
 
